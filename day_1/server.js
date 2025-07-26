b089038c7a16b2feb8ac403190da7a285581d59f
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-07-26 23:47:07 +0530

    created my first server

--- day_1/server.js
+++ day_1/server.js
@@ -3,4 +3,18 @@
 // const fs = require('fs'); // Importing the file system module
 
 // PACKAGE -- is a collection of modules that can be installed from the npm registry;
-// const express = require('express'); // Importing the express package
+// const express = require('express'); // Importing the express package u need to do npm install express
+
+
+// Import the built-in 'http' module to create an HTTP server
+const http = require('http');
+
+// Create an HTTP server instance
+// This server will be able to handle incoming requests and send responses
+const server = http.createServer();
+
+// Make the server listen for incoming requests on port 3000
+// Once the server starts successfully, log a confirmation message to the console
+server.listen(3000, () => {
+  console.log('✅ Server is up and running at http://localhost:3000');
+});
