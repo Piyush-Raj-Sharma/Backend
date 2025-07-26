634a1c20cd491bc1ac3dcbe2b8b459655105eca0
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-07-26 23:24:15 +0530

    first commit

---
+++ day_1/server.js
@@ -0,0 +1,6 @@
+// MODULES -- are some sort of libraries that we can use in our code and is provide by Node.js;
+// const http = require('http'); // Importing the http module
+// const fs = require('fs'); // Importing the file system module
+
+// PACKAGE -- is a collection of modules that can be installed from the npm registry;
+// const express = require('express'); // Importing the express package
