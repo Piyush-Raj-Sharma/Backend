0cbd35914af366d96a830fd9f632bab68c00276b
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-08-18 23:47:06 +0530

    first post method API & used a middleware to parse JSON bodies

--- day_1/server.js
+++ day_1/server.js
@@ -9,6 +9,25 @@ app.get('/about', (req, res)=>{
     res.send('Welcome to the About page!')
 })
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.post('notes', (req, res) => {
+    req.body; // Access the JSON body of the request
+    res.send('Note received!');
+    console.log(req.body);
+    
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
+
+// npm --> Node Package Manager
+
+// first do -> npm init -y
+// npm i express
+
+// npx nodemon server.js  -> helps to restart the server automatically on changes
+// npx nodemon server.js --exec "node --inspect" -> for debugging with Chrome   
+// npx nodemon server.js --exec "node --inspect-brk" -> for debugging with Chrome and break at the start
+
