39a015044b03d6fc780474a56cea3273f42d6763
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-09-30 00:19:19 +0530

    used a middleware between app & routes

--- middleware/src/app.js
+++ middleware/src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const indexRoutes = require('../routes/index.routes');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log("This is a middleware between app & routes");
+    next()
+})
+
 app.use('/', indexRoutes)
 
 
