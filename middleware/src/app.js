0223e1a695dd133c79424d62f025f3e2b91f0b4c
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-09-30 00:21:12 +0530

    add info

--- middleware/src/app.js
+++ middleware/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log("This is a middleware between app & routes");
-    next()
+    next() //without next the Middleware will not allow the request to move forward 
 })
 
 app.use('/', indexRoutes)
