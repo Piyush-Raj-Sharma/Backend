4343589fc5dea69ddb26d217cac89a4248ac5631
piyush-raj-sharma <rpiyush1357@gmail.com>
2025-10-01 01:20:39 +0530

    starting JWT - Authenticiation

--- middleware/routes/index.routes.js
+++ middleware/routes/index.routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.use((req, res, next) => {
+    console.log("This middleware is between routes & API");
+    next(); //without next the Middleware will not allow the request to move forward 
+})
+
+router.get('/   ', (req, res) => {
     res.json({
         message: "Welcome to Middlewares",
     })
